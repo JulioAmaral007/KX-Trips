898b446414cdd7a9e9372c9284975b5cecd14655
Júlio Cézar <captaingamergroot@gmail.com>
2023-07-05 15:59:45 -0300

    feat: update database schema

---
+++ src/app/about/components/Trips.tsx
@@ -0,0 +1,16 @@
+import { prisma } from "@/lib/prisma"
+
+const getTrips = async () => {
+   const trips = await prisma.trip.findMany({})
+
+   return trips
+}
+
+export default async function Trips() {
+   const data = await getTrips()
+
+   console.log({ data })
+   return (
+      <div>Trips</div>
+   )
+}
