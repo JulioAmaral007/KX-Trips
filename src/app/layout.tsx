7bdbe13e1f84dbe456bcd4a0ad6576da968f6e1d
Júlio Cézar <captaingamergroot@gmail.com>
2023-07-06 10:23:03 -0300

    feat: add initial search trip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,38 @@
-import { NextAuthProvider } from "@/providers/auth";
-import { Poppins } from "next/font/google";
-import "./globals.css";
+import React from 'react'
 
-const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "600", "700", "800", "900"] });
+import { Poppins } from 'next/font/google'
+
+import { Header } from '../components/Header'
+
+import { NextAuthProvider } from '@/providers/auth'
+
+import { Footer } from '@/components/Footer'
+import './globals.css'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800', '900'],
+})
 
 export const metadata = {
-  title: "Amaral Trips",
-  description: "Sistema de Reserva de Viagens TOP!",
-};
+  title: 'Amaral Trips',
+  description: 'Sistema de Reserva de Viagens TOP!',
+}
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body className={poppins.className}>
         <NextAuthProvider>
+          <Header />
           {children}
+          <Footer />
         </NextAuthProvider>
       </body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-"use client";
-
-import { useSession } from "next-auth/react";
-
+import { TripSearch } from './components/TripSearch'
 
 export default function Home() {
-
-  const { data } = useSession()
-
   return (
-    <div >
-
+    <div>
+      <TripSearch />
     </div>
-  );
+  )
 }
