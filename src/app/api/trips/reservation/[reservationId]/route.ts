3f2b4c56d6907747ec42eaa41232073e58bc086d
Julio Cezar <captaingamergroot@gmail.com>
2023-11-17 15:38:24 -0300

    feat: try again

--- src/app/api/trips/reservation/[reservationId]/route.ts
+++ src/app/api/trips/reservation/[reservationId]/route.ts
@@ -1,24 +1,24 @@
-import { prismaClient } from '@/lib/prisma'
+ import { prismaClient } from '@/lib/prisma'
 import { NextResponse } from 'next/server'
 
-export async function DELETE(
-  _request: Request,
-  { params: { reservationId } }: { params: { reservationId: string } },
-) {
-  if (!reservationId) {
-    return {
-      status: 400,
-      body: {
-        message: 'Missing reservationId',
-      },
-    }
-  }
+ export async function DELETE(
+   _request: Request,
+   { params: { reservationId } }: { params: { reservationId: string } },
+ ) {
+   if (!reservationId) {
+     return {
+       status: 400,
+       body: {
+         message: 'Missing reservationId',
+       },
+     }
+   }
 
-  const reservation = await prismaClient.tripReservation.delete({
-    where: {
-      id: reservationId,
-    },
-  })
+   const reservation = await prismaClient.tripReservation.delete({
+     where: {
+       id: reservationId,
+     },
+   })
 
-  return new NextResponse(JSON.stringify(reservation), { status: 200 })
-}
+   return new NextResponse(JSON.stringify(reservation), { status: 200 })
+ }
